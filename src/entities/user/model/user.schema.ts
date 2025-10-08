969dbed49148f4fe50ca373bc166d30e7b5b8df7
Efim Shikanov <shikanovefim@gmail.com>
2025-10-08 20:49:03 +0500

    fix: remove comment

--- src/entities/user/model/user.schema.ts
+++ src/entities/user/model/user.schema.ts
@@ -3,14 +3,12 @@ import { UserRole } from './user.types';
 
 export const CreateUserSchema = z.object({
   email: z.email('Невалидный формат email'),
-  phone: z
-    .string()
-    .regex(/^\+?\d{10,15}$/, 'Невалидный формат номера телефона'),
+  phone: z.string().regex(/^\+?\d{11}$/, 'Невалидный формат номера телефона'),
   name: z
     .string()
     .min(2, 'Имя должно содержать минимум 2 символа')
     .max(100, 'Имя не должно превышать 100 символов'),
-  role: z.enum(UserRole), // Использование z.nativeEnum для TypeScript enum
+  role: z.enum(UserRole),
 });
 
 export const UpdateUserSchema = CreateUserSchema.extend({
